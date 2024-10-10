75461e1a998482a1105528839309988ce8c2e380
Md Araful islam Raju <143991599+araful39@users.noreply.github.com>
2024-10-11 00:44:41 +0600

    Update constant.dart

--- lib/constant.dart
+++ lib/constant.dart
@@ -4,3 +4,4 @@ import 'package:flutter/material.dart';
 const String google_api_key="AIzaSyDQ2c_pOSOFYSjxGMwkFvCVWKjYOM9siow";
 const Color primaryColor=Colors.orange;
 const double defaultPadding=16.0;
+const String name="Md Araful islam";
