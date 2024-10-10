0dd2363b2dfc8ac73c6f9d6b940feb1827fa27c7
Md Araful islam Raju <143991599+araful39@users.noreply.github.com>
2024-10-11 01:11:04 +0600

    Update constant.dart

--- lib/constant.dart
+++ lib/constant.dart
@@ -5,3 +5,4 @@ const String google_api_key="AIzaSyDQ2c_pOSOFYSjxGMwkFvCVWKjYOM9siow";
 const Color primaryColor=Colors.orange;
 const double defaultPadding=16.0;
 const String name="Md Araful islam Raju";
+const String name3="Raju";
