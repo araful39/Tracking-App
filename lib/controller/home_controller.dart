8b52c5214addfaf1afe39505ae72c0d93183e87d
Md Araful islam Raju <143991599+araful39@users.noreply.github.com>
2024-10-10 08:35:46 +0600

    polyline point package add

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -4,4 +4,8 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 class HomeController extends GetxController {
   LatLng sourceLocation = const LatLng(23.866554, 90.384875);
   LatLng destinationLocation = const LatLng(23.868819, 90.381597);
+
+  void getPolyPoints(){
+
+  }
 }
